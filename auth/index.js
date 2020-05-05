eeaead7e145647cff4cead280ae6487d619244c5
fabianmoronzirfas <fabianmoronzirfas@protonmail.ch>
2020-05-05 18:11:54 +0200

    setup(auth0): Adds new auth0 tenant removes sensitive data from source and adds them as env secrets

--- auth/index.js
+++ auth/index.js
@@ -2,15 +2,15 @@ var jwt = require('express-jwt');
 var jwks = require('jwks-rsa');
 
 var jwtCheck = jwt({
-      secret: jwks.expressJwtSecret({
-        cache: true,
-        rateLimit: true,
-        jwksRequestsPerMinute: 5,
-        jwksUri: 'https://technologiestiftung.eu.auth0.com/.well-known/jwks.json'
-    }),
-    audience: 'https://trees-express-now.fabiandinklage.now.sh/private/',
-    issuer: 'https://technologiestiftung.eu.auth0.com/',
-    algorithms: ['RS256']
+  secret: jwks.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: process.env.jwksuri,
+  }),
+  audience: process.env.audience,
+  issuer: process.env.issuer,
+  algorithms: ['RS256'],
 });
 
 module.exports = jwtCheck;
