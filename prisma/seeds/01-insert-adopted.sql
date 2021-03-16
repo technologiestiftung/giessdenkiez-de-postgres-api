fbc5211593d874e4cea72b80d7a09a25cd8131b8
ff6347 <fabianmoronzirfas@protonmail.ch>
2021-03-16 20:42:52 +0100

    feat(db): Add prisma seed for faster development
    Uses the prisma seed function to add at least some trees into the DB

---
+++ prisma/seeds/01-insert-adopted.sql
@@ -0,0 +1,11 @@
+INSERT INTO
+  "public"."trees_adopted" ("tree_id", "uuid")
+VALUES
+  ('_01', 'auth0|abc'),
+  ('_02', 'auth0|abc'),
+  ('_01', 'auth0|abc'),
+  ('_kof3lzpk2', 'auth0|abc'),
+  ('_0p47pcxxy', 'auth0|abc'),
+  ('_2tbwepv9g', 'auth0|def'),
+  ('_g78oc2i3q', 'auth0|def'),
+  ('_25qe9e6s9', 'auth0|abc');
