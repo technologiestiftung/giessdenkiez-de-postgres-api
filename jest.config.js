e35dd495a9e32997632f70666e0c96b209607333
ff6347 <fabianmoronzirfas@protonmail.ch>
2021-03-16 21:44:03 +0100

    test: Add a docker warning to the tests

--- jest.config.js
+++ jest.config.js
@@ -7,7 +7,7 @@ module.exports = defaultConfig;
 
 module.exports = utlities.merge(defaultConfig, {
   testEnvironment: path.join(__dirname, "prisma", "prisma-test-environment.js"),
-  // setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
   collectCoverageFrom: ["api/**/*.{ts,tsx}", "api/*.{ts,tsx}"],
   // globalSetup : "./test/jest.global-setup.js",
   // globalTeardown : "./test/jest.global-teardown.js",
---
+++ jest.setup.js
@@ -0,0 +1,7 @@
+console.warn(
+  `did you run
+
+docker-compose -f docker-compose.test.yml up -d
+
+before your running your tests?`,
+);
