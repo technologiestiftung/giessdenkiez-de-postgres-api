cb77a98117162ac0d51f1b83003b673b06e2267e
ff6347 <hash@ff6347.email>
2023-01-12 18:30:06 +0100

    chore: Update types

--- api/_types/database.ts
+++ api/_types/database.ts
@@ -1,249 +1,253 @@
 export type Json =
-	| string
-	| number
-	| boolean
-	| null
-	| { [key: string]: Json }
-	| Json[];
+  | string
+  | number
+  | boolean
+  | null
+  | { [key: string]: Json }
+  | Json[]
 
 export interface Database {
-	public: {
-		Tables: {
-			profiles: {
-				Row: {
-					id: string;
-					username: string | null;
-				};
-				Insert: {
-					id: string;
-					username?: string | null;
-				};
-				Update: {
-					id?: string;
-					username?: string | null;
-				};
-			};
-			radolan_data: {
-				Row: {
-					measured_at: string | null;
-					value: number | null;
-					geom_id: number | null;
-					id: number;
-				};
-				Insert: {
-					measured_at?: string | null;
-					value?: number | null;
-					geom_id?: number | null;
-					id?: number;
-				};
-				Update: {
-					measured_at?: string | null;
-					value?: number | null;
-					geom_id?: number | null;
-					id?: number;
-				};
-			};
-			radolan_geometry: {
-				Row: {
-					geometry: unknown | null;
-					centroid: unknown | null;
-					id: number;
-				};
-				Insert: {
-					geometry?: unknown | null;
-					centroid?: unknown | null;
-					id?: number;
-				};
-				Update: {
-					geometry?: unknown | null;
-					centroid?: unknown | null;
-					id?: number;
-				};
-			};
-			radolan_harvester: {
-				Row: {
-					collection_date: string | null;
-					start_date: string | null;
-					end_date: string | null;
-					id: number;
-				};
-				Insert: {
-					collection_date?: string | null;
-					start_date?: string | null;
-					end_date?: string | null;
-					id?: number;
-				};
-				Update: {
-					collection_date?: string | null;
-					start_date?: string | null;
-					end_date?: string | null;
-					id?: number;
-				};
-			};
-			radolan_temp: {
-				Row: {
-					geometry: unknown | null;
-					value: number | null;
-					measured_at: string | null;
-					id: number;
-				};
-				Insert: {
-					geometry?: unknown | null;
-					value?: number | null;
-					measured_at?: string | null;
-					id?: number;
-				};
-				Update: {
-					geometry?: unknown | null;
-					value?: number | null;
-					measured_at?: string | null;
-					id?: number;
-				};
-			};
-			trees: {
-				Row: {
-					id: string;
-					lat: string | null;
-					lng: string | null;
-					artdtsch: string | null;
-					artbot: string | null;
-					gattungdeutsch: string | null;
-					gattung: string | null;
-					strname: string | null;
-					hausnr: string | null;
-					zusatz: string | null;
-					pflanzjahr: string | null;
-					standalter: string | null;
-					kronedurch: string | null;
-					stammumfg: string | null;
-					type: string | null;
-					baumhoehe: string | null;
-					bezirk: string | null;
-					eigentuemer: string | null;
-					adopted: string | null;
-					watered: string | null;
-					radolan_sum: number | null;
-					radolan_days: number[] | null;
-					geom: unknown | null;
-					standortnr: string | null;
-					kennzeich: string | null;
-					caretaker: string | null;
-					gmlid: string | null;
-				};
-				Insert: {
-					id: string;
-					lat?: string | null;
-					lng?: string | null;
-					artdtsch?: string | null;
-					artbot?: string | null;
-					gattungdeutsch?: string | null;
-					gattung?: string | null;
-					strname?: string | null;
-					hausnr?: string | null;
-					zusatz?: string | null;
-					pflanzjahr?: string | null;
-					standalter?: string | null;
-					kronedurch?: string | null;
-					stammumfg?: string | null;
-					type?: string | null;
-					baumhoehe?: string | null;
-					bezirk?: string | null;
-					eigentuemer?: string | null;
-					adopted?: string | null;
-					watered?: string | null;
-					radolan_sum?: number | null;
-					radolan_days?: number[] | null;
-					geom?: unknown | null;
-					standortnr?: string | null;
-					kennzeich?: string | null;
-					caretaker?: string | null;
-					gmlid?: string | null;
-				};
-				Update: {
-					id?: string;
-					lat?: string | null;
-					lng?: string | null;
-					artdtsch?: string | null;
-					artbot?: string | null;
-					gattungdeutsch?: string | null;
-					gattung?: string | null;
-					strname?: string | null;
-					hausnr?: string | null;
-					zusatz?: string | null;
-					pflanzjahr?: string | null;
-					standalter?: string | null;
-					kronedurch?: string | null;
-					stammumfg?: string | null;
-					type?: string | null;
-					baumhoehe?: string | null;
-					bezirk?: string | null;
-					eigentuemer?: string | null;
-					adopted?: string | null;
-					watered?: string | null;
-					radolan_sum?: number | null;
-					radolan_days?: number[] | null;
-					geom?: unknown | null;
-					standortnr?: string | null;
-					kennzeich?: string | null;
-					caretaker?: string | null;
-					gmlid?: string | null;
-				};
-			};
-			trees_adopted: {
-				Row: {
-					uuid: string | null;
-					tree_id: string;
-					id: number;
-				};
-				Insert: {
-					uuid?: string | null;
-					tree_id: string;
-					id?: number;
-				};
-				Update: {
-					uuid?: string | null;
-					tree_id?: string;
-					id?: number;
-				};
-			};
-			trees_watered: {
-				Row: {
-					time: string;
-					uuid: string | null;
-					amount: number | null;
-					timestamp: string | null;
-					username: string | null;
-					tree_id: string;
-					id: number;
-				};
-				Insert: {
-					time: string;
-					uuid?: string | null;
-					amount?: number | null;
-					timestamp?: string | null;
-					username?: string | null;
-					tree_id: string;
-					id?: number;
-				};
-				Update: {
-					time?: string;
-					uuid?: string | null;
-					amount?: number | null;
-					timestamp?: string | null;
-					username?: string | null;
-					tree_id?: string;
-					id?: number;
-				};
-			};
-		};
-		Views: {
-			[_ in never]: never;
-		};
-		Functions: {
-			[_ in never]: never;
-		};
-		Enums: {
-			[_ in never]: never;
-		};
-	};
+  public: {
+    Tables: {
+      profiles: {
+        Row: {
+          id: string
+          username: string | null
+        }
+        Insert: {
+          id: string
+          username?: string | null
+        }
+        Update: {
+          id?: string
+          username?: string | null
+        }
+      }
+      radolan_data: {
+        Row: {
+          geom_id: number | null
+          id: number
+          measured_at: string | null
+          value: number | null
+        }
+        Insert: {
+          geom_id?: number | null
+          id?: number
+          measured_at?: string | null
+          value?: number | null
+        }
+        Update: {
+          geom_id?: number | null
+          id?: number
+          measured_at?: string | null
+          value?: number | null
+        }
+      }
+      radolan_geometry: {
+        Row: {
+          centroid: unknown | null
+          geometry: unknown | null
+          id: number
+        }
+        Insert: {
+          centroid?: unknown | null
+          geometry?: unknown | null
+          id?: number
+        }
+        Update: {
+          centroid?: unknown | null
+          geometry?: unknown | null
+          id?: number
+        }
+      }
+      radolan_harvester: {
+        Row: {
+          collection_date: string | null
+          end_date: string | null
+          id: number
+          start_date: string | null
+        }
+        Insert: {
+          collection_date?: string | null
+          end_date?: string | null
+          id?: number
+          start_date?: string | null
+        }
+        Update: {
+          collection_date?: string | null
+          end_date?: string | null
+          id?: number
+          start_date?: string | null
+        }
+      }
+      radolan_temp: {
+        Row: {
+          geometry: unknown | null
+          id: number
+          measured_at: string | null
+          value: number | null
+        }
+        Insert: {
+          geometry?: unknown | null
+          id?: number
+          measured_at?: string | null
+          value?: number | null
+        }
+        Update: {
+          geometry?: unknown | null
+          id?: number
+          measured_at?: string | null
+          value?: number | null
+        }
+      }
+      trees: {
+        Row: {
+          adopted: string | null
+          artbot: string | null
+          artdtsch: string | null
+          baumhoehe: string | null
+          bezirk: string | null
+          caretaker: string | null
+          eigentuemer: string | null
+          gattung: string | null
+          gattungdeutsch: string | null
+          geom: unknown | null
+          gmlid: string | null
+          hausnr: string | null
+          id: string
+          kennzeich: string | null
+          kronedurch: string | null
+          lat: string | null
+          lng: string | null
+          pflanzjahr: string | null
+          radolan_days: number[] | null
+          radolan_sum: number | null
+          stammumfg: string | null
+          standalter: string | null
+          standortnr: string | null
+          strname: string | null
+          type: string | null
+          watered: string | null
+          zusatz: string | null
+        }
+        Insert: {
+          adopted?: string | null
+          artbot?: string | null
+          artdtsch?: string | null
+          baumhoehe?: string | null
+          bezirk?: string | null
+          caretaker?: string | null
+          eigentuemer?: string | null
+          gattung?: string | null
+          gattungdeutsch?: string | null
+          geom?: unknown | null
+          gmlid?: string | null
+          hausnr?: string | null
+          id: string
+          kennzeich?: string | null
+          kronedurch?: string | null
+          lat?: string | null
+          lng?: string | null
+          pflanzjahr?: string | null
+          radolan_days?: number[] | null
+          radolan_sum?: number | null
+          stammumfg?: string | null
+          standalter?: string | null
+          standortnr?: string | null
+          strname?: string | null
+          type?: string | null
+          watered?: string | null
+          zusatz?: string | null
+        }
+        Update: {
+          adopted?: string | null
+          artbot?: string | null
+          artdtsch?: string | null
+          baumhoehe?: string | null
+          bezirk?: string | null
+          caretaker?: string | null
+          eigentuemer?: string | null
+          gattung?: string | null
+          gattungdeutsch?: string | null
+          geom?: unknown | null
+          gmlid?: string | null
+          hausnr?: string | null
+          id?: string
+          kennzeich?: string | null
+          kronedurch?: string | null
+          lat?: string | null
+          lng?: string | null
+          pflanzjahr?: string | null
+          radolan_days?: number[] | null
+          radolan_sum?: number | null
+          stammumfg?: string | null
+          standalter?: string | null
+          standortnr?: string | null
+          strname?: string | null
+          type?: string | null
+          watered?: string | null
+          zusatz?: string | null
+        }
+      }
+      trees_adopted: {
+        Row: {
+          id: number
+          tree_id: string
+          uuid: string | null
+        }
+        Insert: {
+          id?: number
+          tree_id: string
+          uuid?: string | null
+        }
+        Update: {
+          id?: number
+          tree_id?: string
+          uuid?: string | null
+        }
+      }
+      trees_watered: {
+        Row: {
+          amount: number | null
+          id: number
+          time: string
+          timestamp: string | null
+          tree_id: string
+          username: string | null
+          uuid: string | null
+        }
+        Insert: {
+          amount?: number | null
+          id?: number
+          time: string
+          timestamp?: string | null
+          tree_id: string
+          username?: string | null
+          uuid?: string | null
+        }
+        Update: {
+          amount?: number | null
+          id?: number
+          time?: string
+          timestamp?: string | null
+          tree_id?: string
+          username?: string | null
+          uuid?: string | null
+        }
+      }
+    }
+    Views: {
+      [_ in never]: never
+    }
+    Functions: {
+      get_watered_and_adopted: {
+        Args: Record<PropertyKey, never>
+        Returns: { tree_id: string; adopted: number; watered: number }[]
+      }
+    }
+    Enums: {
+      [_ in never]: never
+    }
+  }
 }
+
