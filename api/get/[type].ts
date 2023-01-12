755db01ffd2b4cc77db8bdcebe5378294809aaf6
ff6347 <hash@ff6347.email>
2023-01-12 18:31:15 +0100

    feat: Implement partial api routes

--- api/get/[type].ts
+++ api/get/[type].ts
@@ -13,14 +13,16 @@ const queryTypes = [
 ];
 
 import { VercelRequest, VercelResponse } from "@vercel/node";
+import { Database } from "../_types/database";
 import setHeaders from "../_utils/set-headers";
+import { setupResponseData } from "../_utils/setup-response";
 import { supabase } from "../_utils/supabase";
 
 // api/[name].ts -> /api/lee
 // req.query.name -> "lee"
 export default async function (
 	request: VercelRequest,
-	response: VercelResponse,
+	response: VercelResponse
 ) {
 	setHeaders(response, "GET");
 	if (request.method === "OPTIONS") {
@@ -30,26 +32,149 @@ export default async function (
 	if (Array.isArray(type)) {
 		return response.status(400).json({ error: "type needs to be a string" });
 	}
+	if (!queryTypes.includes(type)) {
+		return response.status(404).json({ error: `invalid route ${type}` });
+	}
 
 	switch (type) {
 		default:
 			return response.status(400).json({ error: "invalid query type" });
-		case "byid":
-			return response.status(200).json({ message: "byid" });
-		case "treesbyids":
-			return response.status(200).json({ message: "treesbyids" });
+		case "byid": {
+			const { id } = request.query;
+			if (Array.isArray(id)) {
+				return response.status(400).json({ error: "id needs to be a string" });
+			}
+			if (!id) {
+				return response.status(400).json({ error: "id query is required" });
+			}
+			const { data: trees, error } = await supabase
+				.from("trees")
+				.select("*")
+				.eq("id", id);
+			if (error) {
+				return response.status(500).json({ error });
+			}
+			if (!trees) {
+				return response.status(404).json({ error: "tree not found" });
+			}
+			const data = setupResponseData({ url: request.url, data: trees, error });
+			return response.status(200).json(data);
+		}
+		case "watered": {
+			const { data: trees_watered, error } = await supabase
+				.from("trees_watered")
+				.select("tree_id");
+			if (error) {
+				return response.status(500).json({ error });
+			}
+			if (!trees_watered) {
+				return response.status(500).json({ error: "trees_watered not found" });
+			}
+			const data = setupResponseData({
+				url: request.url,
+				data: trees_watered,
+				error,
+			});
+			return response.status(200).json(data);
+		}
+		case "treesbyids": {
+			const { tree_ids } = request.query;
+			if (Array.isArray(tree_ids)) {
+				return response.status(400).json({
+					error: "ids needs to be a a comma separated list of tree_ids",
+				});
+			}
+			if (!tree_ids) {
+				return response.status(400).json({ error: "ids query is required" });
+			}
+			const trimmed_tree_ids = tree_ids.split(",").map((id) => id.trim());
+			const { data: trees, error } = await supabase
+				.from("trees")
+				.select("*")
+				.in("id", trimmed_tree_ids);
+			if (error) {
+				return response.status(500).json({ error });
+			}
+			if (!trees) {
+				return response.status(500).json({ error: "trees not found" });
+			}
+
+			const data = setupResponseData({ url: request.url, data: trees, error });
+			return response.status(200).json(data);
+		}
+		case "wateredandadopted": {
+			const { data: trees_watered_and_adopted, error } = await supabase.rpc(
+				"get_watered_and_adopted"
+			);
+			if (error) {
+				return response.status(500).json({ error });
+			}
+			if (!trees_watered_and_adopted) {
+				return response.status(500).json({
+					error: "function trees_watered_and_adopted not found",
+				});
+			}
+			const data = setupResponseData({
+				url: request.url,
+				data: trees_watered_and_adopted,
+				error,
+			});
+			return response.status(200).json(data);
+		}
+		case "all": {
+			let { limit: limit_str, offset: offset_str } = request.query;
+			if (Array.isArray(limit_str)) {
+				return response
+					.status(400)
+					.json({ error: "limit needs to be a string" });
+			}
+			if (Array.isArray(offset_str)) {
+				return response
+					.status(400)
+					.json({ error: "offset needs to be a string" });
+			}
+			if (!limit_str) {
+				limit_str = "100";
+			}
+			if (!offset_str) {
+				offset_str = "0";
+			}
+			const limit = parseInt(limit_str, 10);
+			const offset = parseInt(offset_str, 10);
+			if (isNaN(limit)) {
+				return response
+					.status(400)
+					.json({ error: "limit needs to be a number" });
+			}
+			if (isNaN(offset)) {
+				return response
+					.status(400)
+					.json({ error: "offset needs to be a number" });
+			}
+			if (limit > 10000) {
+				return response.status(400).json({
+					error: "limit needs to be smaller than 10000",
+				});
+			}
+			const { data: trees, error } = await supabase
+				.from("trees")
+				.select("id,lat,lng,radolan_sum")
+				.range(offset, offset + limit);
+			if (error) {
+				return response.status(500).json({ error });
+			}
+			if (!trees) {
+				return response.status(500).json({ error: "trees not found" });
+			}
+			const data = setupResponseData({ url: request.url, data: trees, error });
+			return response.status(200).json(data);
+		}
 		case "adopted":
 			return response.status(200).json({ message: "adopted" });
 		case "countbyage":
 			return response.status(200).json({ message: "countbyage" });
-		case "watered":
-			return response.status(200).json({ message: "watered" });
-		case "all":
-			return response.status(200).json({ message: "all" });
 		case "istreeadopted":
 			return response.status(200).json({ message: "istreeadopted" });
-		case "wateredandadopted":
-			return response.status(200).json({ message: "wateredandadopted" });
 		case "byage":
 			return response.status(200).json({ message: "byage" });
 		case "lastwatered":
