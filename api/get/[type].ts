f8d4423d26a38ec74f2bbe76b13e858770cd8645
ff6347 <hash@ff6347.email>
2023-01-18 22:33:06 +0100

    refactor: Unite variable naming

--- api/get/[type].ts
+++ api/get/[type].ts
@@ -4,6 +4,7 @@ import { setupResponseData } from "../_utils/setup-response";
 import { supabase } from "../_utils/supabase";
 import type { Point } from "geojson";
 import { verifyRequest } from "../_utils/verify";
+import { PostgrestError } from "@supabase/supabase-js";
 const queryTypes = [
 	"byid",
 	"treesbyids",
@@ -17,6 +18,14 @@ const queryTypes = [
 	"lastwatered",
 	"wateredbyuser",
 ];
+
+function returnErrorResponse(
+	response: VercelResponse,
+	error: PostgrestError | null,
+	status: number
+) {
+	return response.status(status).json({ error });
+}
 
 // api/[name].ts -> /api/lee
 // req.query.name -> "lee"
@@ -47,35 +56,39 @@ export default async function handler(
 			if (!id) {
 				return response.status(400).json({ error: "id query is required" });
 			}
-			const { data: trees, error } = await supabase
+			const { data, error } = await supabase
 				.from("trees")
 				.select("*")
 				.eq("id", id);
-			if (error) {
-				return response.status(500).json({ error });
+
+			returnErrorResponse(response, error, 500);
+
+			if (!data) {
+				return response.status(500).json({ error: "tree not found" });
 			}
-			if (!trees) {
-				return response.status(404).json({ error: "tree not found" });
-			}
-			const data = setupResponseData({ url: request.url, data: trees, error });
-			return response.status(200).json(data);
-		}
-		case "watered": {
-			const { data: trees_watered, error } = await supabase
-				.from("trees_watered")
-				.select("tree_id");
-			if (error) {
-				return response.status(500).json({ error });
-			}
-			if (!trees_watered) {
-				return response.status(500).json({ error: "trees_watered not found" });
-			}
-			const data = setupResponseData({
+			const result = setupResponseData({
 				url: request.url,
-				data: trees_watered,
+				data,
 				error,
 			});
-			return response.status(200).json(data);
+			return response.status(200).json(result);
+		}
+		case "watered": {
+			const { data, error } = await supabase
+				.from("trees_watered")
+				.select("tree_id");
+
+			returnErrorResponse(response, error, 500);
+
+			if (!data) {
+				return response.status(500).json({ error: "trees_watered not found" });
+			}
+			const result = setupResponseData({
+				url: request.url,
+				data,
+				error,
+			});
+			return response.status(200).json(result);
 		}
 		case "treesbyids": {
 			const { tree_ids } = request.query;
@@ -90,38 +103,40 @@ export default async function handler(
 					.json({ error: "tree_ids query is required" });
 			}
 			const trimmed_tree_ids = tree_ids.split(",").map((id) => id.trim());
-			const { data: trees, error } = await supabase
+			const { data, error } = await supabase
 				.from("trees")
 				.select("*")
 				.in("id", trimmed_tree_ids);
-			if (error) {
-				return response.status(500).json({ error });
-			}
-			if (!trees) {
+
+			returnErrorResponse(response, error, 500);
+
+			if (!data) {
 				return response.status(500).json({ error: "trees not found" });
 			}
 
-			const data = setupResponseData({ url: request.url, data: trees, error });
-			return response.status(200).json(data);
+			const result = setupResponseData({
+				url: request.url,
+				data,
+				error,
+			});
+			return response.status(200).json(result);
 		}
 		case "wateredandadopted": {
-			const { data: trees_watered_and_adopted, error } = await supabase.rpc(
-				"get_watered_and_adopted"
-			);
-			if (error) {
-				return response.status(500).json({ error });
-			}
-			if (!trees_watered_and_adopted) {
+			const { data, error } = await supabase.rpc("get_watered_and_adopted");
+
+			returnErrorResponse(response, error, 500);
+
+			if (!data) {
 				return response.status(500).json({
 					error: "function trees_watered_and_adopted not found",
 				});
 			}
-			const data = setupResponseData({
+			const result = setupResponseData({
 				url: request.url,
-				data: trees_watered_and_adopted,
+				data,
 				error,
 			});
-			return response.status(200).json(data);
+			return response.status(200).json(result);
 		}
 		case "all": {
 			let { limit: limit_str, offset: offset_str } = request.query;
@@ -158,7 +173,7 @@ export default async function handler(
 					error: "limit needs to be smaller than 10000",
 				});
 			}
-			const { data: trees, error } = await supabase
+			const { data, error } = await supabase
 				.from("trees")
 				.select<
 					"id,radolan_sum,geom",
@@ -171,15 +186,15 @@ export default async function handler(
 					}
 				>("id,radolan_sum,geom")
 				.range(offset, offset + limit);
-			if (error) {
-				return response.status(500).json({ error });
-			}
-			if (!trees) {
+
+			returnErrorResponse(response, error, 500);
+
+			if (!data) {
 				return response.status(500).json({ error: "trees not found" });
 			}
 			// to match the old structure we need to transform the data a little
 			// FIXME: [GDK-217] API (with supabase): GET "all" should work with result that is returned without transforming the data into the current structure
-			const watered = trees.map((tree) => {
+			const watered = data.map((tree) => {
 				return [
 					tree.id,
 					tree.geom.coordinates[0] ? tree.geom.coordinates[0] : 0,
@@ -188,35 +203,35 @@ export default async function handler(
 				];
 			});
 
-			const data = setupResponseData({
+			const result = setupResponseData({
 				url: request.url,
 				data: watered,
 				error,
 			});
-			return response.status(200).json(data);
+			return response.status(200).json(result);
 		}
 		case "countbyage": {
-			const { start: start_str, end: end_str } = request.query;
-			if (!start_str) {
+			const { start: startStr, end: endStr } = request.query;
+			if (!startStr) {
 				return response.status(400).json({ error: "start query is required" });
 			}
-			if (!end_str) {
+			if (!endStr) {
 				return response.status(400).json({ error: "end query is required" });
 			}
-			if (Array.isArray(start_str)) {
+			if (Array.isArray(startStr)) {
 				return response
 					.status(400)
 					.json({ error: "start needs to be a string" });
 			}
-			if (Array.isArray(end_str)) {
+			if (Array.isArray(endStr)) {
 				return response.status(400).json({ error: "end needs to be a string" });
 			}
-			const start = isNaN(parseInt(start_str, 10))
+			const start = isNaN(parseInt(startStr, 10))
 				? undefined
-				: parseInt(start_str, 10);
-			const end = isNaN(parseInt(end_str, 10))
+				: parseInt(startStr, 10);
+			const end = isNaN(parseInt(endStr, 10))
 				? undefined
-				: parseInt(end_str, 10);
+				: parseInt(endStr, 10);
 			if (start === undefined) {
 				return response
 					.status(400)
@@ -230,43 +245,43 @@ export default async function handler(
 				start_year: start,
 				end_year: end,
 			});
-			if (error) {
-				return response.status(500).json({ error });
-			}
+
+			returnErrorResponse(response, error, 500);
+
 			if (!count) {
 				return response
 					.status(500)
 					.json({ error: "could not call function count_by_age" });
 			}
-			const data = setupResponseData({
+			const result = setupResponseData({
 				url: request.url,
 				data: { count },
 				error,
 			});
-			return response.status(200).json(data);
+			return response.status(200).json(result);
 		}
 		case "byage": {
-			const { start: start_str, end: end_str } = request.query;
-			if (!start_str) {
+			const { start: startStr, end: endStr } = request.query;
+			if (!startStr) {
 				return response.status(400).json({ error: "start query is required" });
 			}
-			if (!end_str) {
+			if (!endStr) {
 				return response.status(400).json({ error: "end query is required" });
 			}
-			if (Array.isArray(start_str)) {
+			if (Array.isArray(startStr)) {
 				return response
 					.status(400)
 					.json({ error: "start needs to be a string" });
 			}
-			if (Array.isArray(end_str)) {
+			if (Array.isArray(endStr)) {
 				return response.status(400).json({ error: "end needs to be a string" });
 			}
-			const start = isNaN(parseInt(start_str, 10))
+			const start = isNaN(parseInt(startStr, 10))
 				? undefined
-				: parseInt(start_str, 10);
-			const end = isNaN(parseInt(end_str, 10))
+				: parseInt(startStr, 10);
+			const end = isNaN(parseInt(endStr, 10))
 				? undefined
-				: parseInt(end_str, 10);
+				: parseInt(endStr, 10);
 			if (start === undefined) {
 				return response
 					.status(400)
@@ -276,24 +291,24 @@ export default async function handler(
 				return response.status(400).json({ error: "end needs to be a number" });
 			}
 
-			const { data: trees, error } = await supabase
+			const { data, error } = await supabase
 				.from("trees")
 				.select("id")
 				.gte("pflanzjahr", start)
 				.lte("pflanzjahr", end);
-			if (error) {
-				return response.status(500).json({ error });
-			}
-			if (!trees) {
+
+			returnErrorResponse(response, error, 500);
+
+			if (!data) {
 				return response.status(500).json({ error: "trees not found" });
 			}
-			const data = setupResponseData({
+			const result = setupResponseData({
 				url: request.url,
-				data: trees,
+				data,
 				error,
 			});
 
-			return response.status(200).json(data);
+			return response.status(200).json(result);
 		}
 		//TODO: [GDK-218] API (with supabase) Should GET lastwatered be only available for authenticated users?
 		case "lastwatered": {
@@ -304,23 +319,23 @@ export default async function handler(
 			if (Array.isArray(id)) {
 				return response.status(400).json({ error: "id needs to be a string" });
 			}
-			const { data: trees, error } = await supabase
+			const { data, error } = await supabase
 				.from("trees_watered")
 				.select("id,timestamp,amount,username,tree_id")
 				.eq("tree_id", id)
 				.order("timestamp", { ascending: false });
-			if (error) {
-				return response.status(500).json({ error });
-			}
-			if (!trees) {
+
+			returnErrorResponse(response, error, 500);
+
+			if (!data) {
 				return response.status(500).json({ error: "trees not found" });
 			}
-			const data = setupResponseData({
+			const result = setupResponseData({
 				url: request.url,
-				data: trees,
+				data,
 				error,
 			});
-			return response.status(200).json(data);
+			return response.status(200).json(result);
 		}
 		// All requests below this line are only available for authenticated users
 
@@ -338,24 +353,23 @@ export default async function handler(
 					.status(400)
 					.json({ error: "uuid needs to be a string" });
 			}
-			const { data: trees, error } = await supabase
+			const { data, error } = await supabase
 				.from("trees_adopted")
 				.select("tree_id,uuid")
 				.eq("uuid", uuid);
 
-			if (error) {
-				return response.status(500).json({ error });
-			}
-			if (!trees) {
+			returnErrorResponse(response, error, 500);
+
+			if (!data) {
 				return response.status(500).json({ error: "trees not found" });
 			}
-			const data = setupResponseData({
+			const result = setupResponseData({
 				url: request.url,
-				data: trees.map((tree) => tree.tree_id),
+				data: data.map((tree) => tree.tree_id),
 				error,
 			});
 
-			return response.status(200).json(data);
+			return response.status(200).json(result);
 		}
 		case "istreeadopted": {
 			const authorized = await verifyRequest(request);
@@ -381,26 +395,25 @@ export default async function handler(
 					.status(400)
 					.json({ error: "tree_id needs to be a string" });
 			}
-			const { data: trees, error } = await supabase
+			const { data, error } = await supabase
 				.from("trees_adopted")
 				.select("uuid,tree_id")
 				.eq("uuid", uuid)
 				.eq("tree_id", id);
 
-			if (error) {
-				return response.status(500).json({ error });
-			}
-			if (!trees) {
+			returnErrorResponse(response, error, 500);
+
+			if (!data) {
 				return response.status(500).json({ error: "trees not found" });
 			}
 
-			const data = setupResponseData({
+			const result = setupResponseData({
 				url: request.url,
-				data: trees.length > 0 ? true : false,
+				data: data.length > 0 ? true : false,
 				error,
 			});
 
-			return response.status(200).json(data);
+			return response.status(200).json(result);
 		}
 
 		case "wateredbyuser": {
@@ -417,23 +430,22 @@ export default async function handler(
 					.status(400)
 					.json({ error: "uuid needs to be a string" });
 			}
-			const { data: trees, error } = await supabase
+			const { data, error } = await supabase
 				.from("trees_watered")
 				.select("*")
 				.eq("uuid", uuid);
 
-			if (error) {
-				return response.status(500).json({ error });
-			}
-			if (!trees) {
+			returnErrorResponse(response, error, 500);
+
+			if (!data) {
 				return response.status(500).json({ error: "trees not found" });
 			}
-			const data = setupResponseData({
+			const result = setupResponseData({
 				url: request.url,
-				data: trees,
+				data,
 				error,
 			});
-			return response.status(200).json(data);
+			return response.status(200).json(result);
 		}
 	}
 }
