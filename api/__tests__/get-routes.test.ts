e593f8e4107f8c5bed90122f107d94f6b1bbadef
ff6347 <hash@ff6347.email>
2023-01-18 21:20:07 +0100

    test: Add basic 200 tests for get route

--- api/__tests__/get-routes.test.ts
+++ api/__tests__/get-routes.test.ts
@@ -1,3 +1,4 @@
+import each from "jest-each";
 import fetch from "cross-fetch";
 import { test, describe, expect } from "@jest/globals";
 import handler from "../get/[type]";
@@ -6,31 +7,140 @@ import {
 	createWateredTrees,
 	truncateTreesWaterd,
 } from "../__test-utils/postgres";
+import { requestTestToken } from "../__test-utils/req-test-token";
 // byid ✓
 // watered ✓
 // all ✓
-// treesbyids
-// adopted
-// countbyage
-// istreeadopted
-// wateredandadopted
-// byage
-// lastwatered
-// wateredbyuser
+// treesbyids  ✓
+// countbyage ✓
+// byage ✓
+// wateredandadopted ✓
+// lastwatered ✓
+//
+// the tests below need auth
+// adopted  ✓
+// istreeadopted ✓
+// wateredbyuser ✓
 
-describe("GET routes (all, byid, watered)", () => {
-	test("should return 404 on invalid route", async () => {
+describe("GET routes snapshot tests default responses", () => {
+	test("should return 200 on wateredbyuser route authenticated", async () => {
+		const token = await requestTestToken();
 		const { server, url } = await createTestServer(
-			{ type: "invalid" },
+			{ type: "wateredbyuser", uuid: "auth0|abc" },
+			handler
+		);
+		const response = await fetch(url, {
+			headers: {
+				authorization: `Bearer ${token}`,
+			},
+		});
+		server.close();
+		const json = await response.json();
+		expect(response.status).toBe(200);
+		expect(json).toMatchSnapshot();
+	});
+	test("should return 200 on istreeadopted route authenticated", async () => {
+		const token = await requestTestToken();
+		const { server, url } = await createTestServer(
+			{ type: "istreeadopted", id: "_210028b9c8", uuid: "auth0|abc" },
+			handler
+		);
+		const response = await fetch(url, {
+			headers: {
+				authorization: `Bearer ${token}`,
+			},
+		});
+		server.close();
+		const json = await response.json();
+		expect(response.status).toBe(200);
+		expect(json).toMatchSnapshot();
+	});
+
+	test("should return 200 on adopted route authenticated", async () => {
+		const token = await requestTestToken();
+		const { server, url } = await createTestServer(
+			{ type: "adopted", uuid: "auth0|abc" },
+			handler
+		);
+		const response = await fetch(url, {
+			headers: {
+				authorization: `Bearer ${token}`,
+			},
+		});
+		server.close();
+		const json = await response.json();
+		expect(response.status).toBe(200);
+		expect(json).toMatchSnapshot();
+	});
+	test("should return 200 on lastwatered route", async () => {
+		const { server, url } = await createTestServer(
+			{ type: "lastwatered", id: "_210028b9c8" },
+			handler
+		);
+		const response = await fetch(url);
+		server.close();
+		const json = await response.json();
+		expect(response.status).toBe(200);
+		expect(json).toMatchSnapshot();
+	});
+	test("should return 200 on wateredandadopted route", async () => {
+		const { server, url } = await createTestServer(
+			{ type: "wateredandadopted" },
+			handler
+		);
+		const response = await fetch(url);
+		server.close();
+		const json = await response.json();
+		expect(response.status).toBe(200);
+		expect(json).toMatchSnapshot();
+	});
+	test("should return 200 on byage route", async () => {
+		const { server, url } = await createTestServer(
+			{ type: "byage", start: "1800", end: "3000" },
 			handler
 		);
 		const response = await fetch(`${url}`);
 		server.close();
-		expect(response.status).toBe(404);
-		expect(response.statusText).toBe("Not Found");
-		expect(await response.json()).toMatchSnapshot();
+		const json = await response.json();
+		expect(response.status).toBe(200);
+		expect(json).toMatchSnapshot();
 	});
 
+	test("should return 200 on wateredandadopted", async () => {
+		const { server, url } = await createTestServer(
+			{ type: "wateredandadopted" },
+			handler
+		);
+		const response = await fetch(url);
+		server.close();
+		const json = await response.json();
+		expect(response.status).toBe(200);
+		expect(json).toMatchSnapshot();
+	});
+	test("should return 200 on countbyage route", async () => {
+		const { server, url } = await createTestServer(
+			{ type: "countbyage", start: "1800", end: "3000" },
+			handler
+		);
+		const response = await fetch(`${url}`);
+		server.close();
+		const json = await response.json();
+		expect(response.status).toBe(200);
+		expect(json).toMatchSnapshot();
+	});
+
+	test("Should return 200 on treesbyid route", async () => {
+		const { server, url } = await createTestServer(
+			{ type: "treesbyids", tree_ids: "_2100294b1f,_210028b9c8" },
+			handler
+		);
+		const response = await fetch(`${url}`);
+		server.close();
+		const json = await response.json();
+
+		expect(response.status).toBe(200);
+		expect(json).toMatchSnapshot();
+	});
 	test("should return 200 on trees_watered watered route", async () => {
 		await truncateTreesWaterd();
 		await createWateredTrees();
@@ -69,4 +179,42 @@ describe("GET routes (all, byid, watered)", () => {
 		expect(response.status).toBe(200);
 		expect(json).toMatchSnapshot();
 	});
+
+	test("should return 404 on invalid route", async () => {
+		const { server, url } = await createTestServer(
+			{ type: "invalid" },
+			handler
+		);
+		const response = await fetch(`${url}`);
+		server.close();
+		expect(response.status).toBe(404);
+		expect(response.statusText).toBe("Not Found");
+		expect(await response.json()).toMatchSnapshot();
+	});
 });
+
+each([
+	[400, "all", { limit: "abc" }, "due to limit being NaN"],
+	[400, "all", { limit: 10000000 }, "due to limit being to large"],
+	[400, "all", { offset: "abc" }, "due to offset being NaN"],
+	[400, "all", { offset: ["1", "2", "3"] }, "due to offset being not a string"],
+]).describe.only(
+	"should return %d on route %s",
+	(
+		statusCode: number,
+		type: string,
+		overrides: Record<string, unknown>,
+		description: string
+	) => {
+		test(`returns ${statusCode} ${description}`, async () => {
+			const { server, url } = await createTestServer(
+				{ type, ...overrides },
+				handler
+			);
+			const response = await fetch(`${url}`);
+			server.close();
+			expect(response.status).toBe(statusCode);
+			console.log(await response.json());
+		});
+	}
+);
