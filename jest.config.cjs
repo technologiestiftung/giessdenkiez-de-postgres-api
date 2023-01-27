70c3b460f5ea84872349971c9cb08015313596b5
ff6347 <hash@ff6347.email>
2023-01-27 11:45:34 +0100

    chore: Configure jest and eslint
    Adds coverage threshold

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,36 +1,45 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-  preset: "ts-jest",
-  transform: {
-    "^.+\\.(ts|tsx|js|jsx)$": [
-      'ts-jest', {
-        tsconfig: 'tsconfig.json',
-        isolatedModules: false
-      },
-    ]
-  },
-  testEnvironment: "node",
-  testMatch: ["**/*.spec.ts", "**/*.test.ts"],
-  collectCoverage: true,
-  collectCoverageFrom: ["api/**/*.ts"],
-  // setupFiles: ["./jest.env.cjs"],
-  setupFilesAfterEnv: ["./jest.setup.cjs"],
-  coveragePathIgnorePatterns: [
-    "<rootDir>/dist/",
-    "<rootDir>/node_modules/",
-    "<rootDir>/api/_deprecated/",
-    "<rootDir>/api/__test-utils",
-    "<rootDir>/api/__tests__",
-    "<rootDir>/api/_types",
-    "<rootDir>/api/.*\\.d\\.ts$",
-    "<rootDir>/api/.*\\.spec\\.ts$",
-    "<rootDir>/api/.*\\.test\\.ts$",
-    "<rootDir>/api/.*\\.test\\.d\\.ts$",
-    "<rootDir>/api/.*\\.spec\\.d\\.ts$",
-  ],
-  testPathIgnorePatterns: [
-    "<rootDir>/dist/",
-    "<rootDir>/node_modules/",
-    "<rootDir>/api/_deprecated/",
-  ],
+	preset: "ts-jest",
+	transform: {
+		"^.+\\.(ts|tsx|js|jsx)$": [
+			"ts-jest",
+			{
+				tsconfig: "tsconfig.json",
+				isolatedModules: false,
+			},
+		],
+	},
+	testEnvironment: "node",
+	testMatch: ["**/*.spec.ts", "**/*.test.ts"],
+	collectCoverage: true,
+	collectCoverageFrom: ["api/**/*.ts"],
+	// setupFiles: ["./jest.env.cjs"],
+	setupFilesAfterEnv: ["./jest.setup.cjs"],
+	coverageThreshold: {
+		global: {
+			branches: 75,
+			functions: 75,
+			lines: 75,
+			statements: 75,
+		},
+	},
+	coveragePathIgnorePatterns: [
+		"<rootDir>/dist/",
+		"<rootDir>/node_modules/",
+		"<rootDir>/api/_deprecated/",
+		"<rootDir>/api/__test-utils",
+		"<rootDir>/api/__tests__",
+		"<rootDir>/api/_types",
+		"<rootDir>/api/.*\\.d\\.ts$",
+		"<rootDir>/api/.*\\.spec\\.ts$",
+		"<rootDir>/api/.*\\.test\\.ts$",
+		"<rootDir>/api/.*\\.test\\.d\\.ts$",
+		"<rootDir>/api/.*\\.spec\\.d\\.ts$",
+	],
+	testPathIgnorePatterns: [
+		"<rootDir>/dist/",
+		"<rootDir>/node_modules/",
+		"<rootDir>/api/_deprecated/",
+	],
 };
