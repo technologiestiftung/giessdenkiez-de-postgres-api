38343d1940ccec1fe712b5fd1b0ceabff8b6ba99
ff6347 <hash@ff6347.email>
2023-01-28 18:43:38 +0100

    style: prettier

--- _utils/set-headers.ts
+++ _utils/set-headers.ts
@@ -2,13 +2,13 @@ import { VercelResponse } from "@vercel/node";
 
 export default function setHeaders(
 	response: VercelResponse,
-	method: "GET" | "POST" | "DELETE",
+	method: "GET" | "POST" | "DELETE"
 ): void {
 	response.setHeader("Access-Control-Allow-Credentials", "true");
 	response.setHeader("Access-Control-Allow-Origin", "*");
 	response.setHeader("Access-Control-Allow-Methods", `${method}, OPTIONS`);
 	response.setHeader(
 		"Access-Control-Allow-Headers",
-		"X-Requested-With, Authorization, Accept, Content-Type",
+		"X-Requested-With, Authorization, Accept, Content-Type"
 	);
 }
