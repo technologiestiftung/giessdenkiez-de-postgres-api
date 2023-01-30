db0eb731a304f53e6f427ca0b2f71067cef2f55d
ff6347 <hash@ff6347.email>
2023-01-30 14:09:16 +0100

    fix(headers): Add headers to default routes

---
+++ api/delete/index.ts
@@ -0,0 +1,19 @@
+import { VercelRequest, VercelResponse } from "@vercel/node";
+import setHeaders from "../../_utils/set-headers.js";
+import { setupResponseData } from "../../_utils/setup-response.js";
+
+export default async function handler(
+	_request: VercelRequest,
+	response: VercelResponse
+) {
+	setHeaders(response, "DELETE");
+	try {
+		return response
+			.status(200)
+			.json(setupResponseData({ message: "its working" }));
+	} catch (error) {
+		return response
+			.status(500)
+			.json(setupResponseData({ error: "its not working" }));
+	}
+}
