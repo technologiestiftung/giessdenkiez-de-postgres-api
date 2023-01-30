92305e219c73e6adca05c8a0a4a0718216ec393d
ff6347 <hash@ff6347.email>
2023-01-30 14:09:23 +0100

    fix(headers): Add headers to default routes

--- api/index.ts
+++ api/index.ts
@@ -1,10 +1,12 @@
 import { VercelRequest, VercelResponse } from "@vercel/node";
+import setHeaders from "../_utils/set-headers.js";
 import { setupResponseData } from "../_utils/setup-response.js";
 
 export default async function handler(
 	_request: VercelRequest,
 	response: VercelResponse
 ) {
+	setHeaders(response, "GET");
 	try {
 		return response
 			.status(200)
