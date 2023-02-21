0934203eed6a14231c930f19c84566743c8414bf
ff6347 <hash@ff6347.email>
2023-02-21 21:06:15 +0100

    fix(limit): Add env for max rows from supabase

--- _utils/envs.ts
+++ _utils/envs.ts
@@ -3,6 +3,7 @@ interface Envs {
 	SUPABASE_URL: string;
 	SUPABASE_ANON_KEY: string;
 	SUPABASE_SERVICE_ROLE_KEY: string;
+	SUPABASE_MAX_ROWS: number;
 
 	JWKS_URI: string;
 	AUDIENCE: string;
@@ -18,6 +19,7 @@ export function getEnvs(): Envs {
 	const SUPABASE_URL = process.env.SUPABASE_URL;
 	const SUPABASE_ANON_KEY = process.env.SUPABASE_ANON_KEY;
 	const SUPABASE_SERVICE_ROLE_KEY = process.env.SUPABASE_SERVICE_ROLE_KEY;
+	const SUPABASE_MAX_ROWS_RAW = process.env.SUPABASE_MAX_ROWS;
 
 	const JWKS_URI = process.env.jwksuri;
 	const AUDIENCE = process.env.audience;
@@ -35,10 +37,17 @@ export function getEnvs(): Envs {
 
 	if (ISSUER === undefined) throw new Error("issuer is undefined");
 
+	if (SUPABASE_MAX_ROWS_RAW === undefined)
+		throw new Error("SUPABASE_MAX_ROWS is undefined");
+	if (isNaN(parseInt(SUPABASE_MAX_ROWS_RAW)))
+		throw new Error("SUPABASE_MAX_ROWS is not parseable to a number");
+	const SUPABASE_MAX_ROWS = parseInt(SUPABASE_MAX_ROWS_RAW);
+
 	return {
 		SUPABASE_URL,
 		SUPABASE_ANON_KEY,
 		SUPABASE_SERVICE_ROLE_KEY,
+		SUPABASE_MAX_ROWS,
 		AUDIENCE,
 		ISSUER,
 		JWKS_URI,
