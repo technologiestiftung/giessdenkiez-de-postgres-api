cac667350e1b00904ff1040328a1f058b45854a6
ff6347 <hash@ff6347.email>
2023-03-16 14:35:32 +0100

    refactor: createLinks to take care of searchParams

--- _utils/create-links.ts
+++ _utils/create-links.ts
@@ -7,26 +7,37 @@ export function createLinks({
 	range,
 	type,
 	method,
-	params,
+	requestUrl,
 }: {
 	limit: number;
 	offset: number;
 	range: ContentRange | null;
 	type: string;
 	method: string;
-	params?: URLSearchParams;
+	requestUrl: string;
 }) {
+	const params = new URLSearchParams(requestUrl.split("?")[1]);
+	// remove limit and offset from searchParams since we pass them as arguments
+	// and they get some precomputing for SUPABASE_MAX_ROWS
+	params.delete("limit");
+	params.delete("offset");
+	const paramsString =
+		params.toString().length > 0 ? "&" + params.toString() : "";
 	const links: Record<string, string> = {};
 	if (range) {
-		if (offset > 0) {
+		// If offset is greater than 0, generate a previous link
+		const isPrevLink = offset > 0;
+		if (isPrevLink) {
 			links.prev = `/${method.toLowerCase()}/${type}?limit=${limit}&offset=${
 				offset - limit < 0 ? 0 : offset - limit
-			}${params ? "&" + params.toString() : ""}`;
+			}${params ? paramsString : ""}`;
 		}
-		if (offset + limit < range.total - 1) {
+		// If the offset is less than the total number of items minus 1, generate a next link
+		const isNextLink = offset + limit < range.total - 1;
+		if (isNextLink) {
 			links.next = `/${method.toLowerCase()}/${type}?limit=${limit}&offset=${
 				offset + limit
-			}${params ? "&" + params.toString() : ""}`;
+			}${params ? paramsString : ""}`;
 		}
 	}
 	return links;
