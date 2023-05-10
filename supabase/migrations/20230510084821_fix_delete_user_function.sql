5fcc5f64533cc021d1c925898069c039f5ea19c3
ff6347 <hash@ff6347.email>
2023-05-10 10:49:07 +0200

    fix(schema): Add public schema to table names
    @JensWinter had problems deleting users from the dashboard
    see #219

---
+++ supabase/migrations/20230510084821_fix_delete_user_function.sql
@@ -0,0 +1,28 @@
+SET check_function_bodies = OFF;
+
+CREATE OR REPLACE FUNCTION public.delete_user()
+	RETURNS TRIGGER
+	LANGUAGE plpgsql
+	SECURITY DEFINER
+	AS $function$
+DECLARE
+	row_count int;
+BEGIN
+	DELETE FROM public.profiles p
+	WHERE p.id = OLD.id;
+	-- 	IF found THEN
+	-- 		GET DIAGNOSTICS row_count = ROW_COUNT;
+	-- 		RAISE NOTICE 'DELETEd % row(s) FROM profiles', row_count;
+	-- 	END IF;
+	UPDATE
+		public.trees_watered
+	SET
+		uuid = NULL,
+		username = NULL
+	WHERE
+		uuid = OLD.id::text;
+	DELETE FROM public.trees_adopted ta
+	WHERE ta.uuid = OLD.id::text;
+	RETURN OLD;
+END;
+$function$
