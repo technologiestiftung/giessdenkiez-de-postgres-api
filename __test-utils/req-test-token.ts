de7f77cf026390b3621b747ba1cf07b7779435e5
ff6347 <hash@ff6347.email>
2023-10-23 12:19:43 +0200

    test(v2): Clean out all v2 code

--- __test-utils/req-test-token.ts
+++ __test-utils/req-test-token.ts
@@ -1,30 +1,5 @@
 import { SignupResponse } from "../_types/user";
 import { SUPABASE_ANON_KEY, SUPABASE_URL } from "./supabase";
-const issuer = process.env.issuer || "";
-const client_id = process.env.client_id || "";
-const client_secret = process.env.client_secret || "";
-const audience = process.env.audience || "";
-
-export async function requestAuth0TestToken() {
-	const response = await fetch(`${issuer}oauth/token`, {
-		method: "POST",
-		headers: {
-			"Content-Type": "application/json",
-		},
-		body: JSON.stringify({
-			client_id,
-			client_secret,
-			audience,
-			grant_type: "client_credentials",
-		}),
-	});
-	if (!response.ok) {
-		const json = await response.text();
-		throw new Error(`Could not get test token, ${json}`);
-	}
-	const json = await response.json();
-	return json.access_token;
-}
 
 export async function requestSupabaseTestToken(
 	email: string,
