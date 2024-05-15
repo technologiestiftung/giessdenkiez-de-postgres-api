95666b9e18cef2079f5ca08eb1593d78e2c7f057
Jonas Jaszkowic <jonas.jaszkowic@ts.berlin>
2024-05-15 13:51:14 +0200

    feat: restrict access to trees_watered

---
+++ supabase/migrations/20240515114451_waterings_via_rpc.sql
@@ -0,0 +1,32 @@
+-- Forbid select access to trees_watered table
+drop policy "Enable select for authenticated users only" on "public"."trees_watered";
+
+-- Allow select waterings for a specific tree only via rpc
+-- SECURITY DEFINER bypasses the RLS policy
+CREATE OR REPLACE FUNCTION public.waterings_for_tree(t_id text)
+ RETURNS TABLE(uuid text, amount numeric, "timestamp" timestamptz, username text, id int4, tree_id text)
+ LANGUAGE plpgsql
+ SECURITY DEFINER
+AS $function$
+BEGIN
+	RETURN query
+	
+	SELECT * FROM trees_watered WHERE trees_watered.tree_id = t_id;
+		
+END;
+$function$;
+
+-- Allow select waterings for a specific user only via rpc
+-- SECURITY DEFINER bypasses the RLS policy
+CREATE OR REPLACE FUNCTION public.waterings_for_user(u_id text)
+ RETURNS TABLE(uuid text, amount numeric, "timestamp" timestamptz, username text, id int4, tree_id text)
+ LANGUAGE plpgsql
+ SECURITY DEFINER
+AS $function$
+BEGIN
+	RETURN query
+	
+	SELECT * FROM trees_watered WHERE trees_watered.uuid = u_id;
+		
+END;
+$function$;
