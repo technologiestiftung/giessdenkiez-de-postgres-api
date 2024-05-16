ac0c51bc48183954525b4bf27c84e1dd503dd943
Jonas Jaszkowic <jonas.jaszkowic@ts.berlin>
2024-05-16 14:52:49 +0200

    fix: rls for delete also needs select

--- supabase/migrations/20240515085246_change_rls_for_insert.sql
+++ supabase/migrations/20240515085246_change_rls_for_insert.sql
@@ -1,12 +1,31 @@
-create policy "Authenticated users can insert their own waterings" on trees_watered
-  for insert to authenticated with check (auth.uid()::text = uuid);
+create policy "Authenticated users can insert their own waterings"
+on trees_watered
+for insert to authenticated
+with check (auth.uid()::text = uuid);
 
-create policy "Authenticated users can delete their own waterings" on trees_watered
-  for delete to authenticated using(auth.uid()::text = uuid);
+create policy "Authenticated users can select their own rows"
+on trees_watered
+for select
+to authenticated
+using (uuid = auth.uid()::text);
 
+create policy "Authenticated users can delete their own rows"
+on trees_watered
+for delete
+to authenticated
+using (uuid = auth.uid()::text);
 
-create policy "Authenticated users can insert their own adoptions" on trees_adopted
-  for insert to authenticated with check (auth.uid()::text = uuid);
+create policy "Authenticated users can insert their own adoptions"
+on trees_adopted
+for insert to authenticated
+with check (auth.uid()::text = uuid);
 
-create policy "Authenticated users can delete their own adoptions" on trees_adopted
-  for delete to authenticated using(auth.uid()::text = uuid);
+create policy "Authenticated users can select their own adoptions"
+on trees_adopted
+for select to authenticated
+using (auth.uid()::text = uuid);
+
+create policy "Authenticated users can delete their own adoptions"
+on trees_adopted
+for delete to authenticated
+using (auth.uid()::text = uuid);
--- supabase/migrations/20240515114451_waterings_via_rpc.sql
+++ supabase/migrations/20240515114451_waterings_via_rpc.sql
@@ -4,14 +4,14 @@ drop policy "Enable select for authenticated users only" on "public"."trees_wate
 -- Allow select waterings for a specific tree only via rpc
 -- SECURITY DEFINER bypasses the RLS policy
 CREATE OR REPLACE FUNCTION public.waterings_for_tree(t_id text)
- RETURNS TABLE(uuid text, amount numeric, "timestamp" timestamptz, username text, id int4, tree_id text)
+ RETURNS TABLE(amount numeric, "timestamp" timestamptz, username text, id int4, tree_id text)
  LANGUAGE plpgsql
  SECURITY DEFINER
 AS $function$
 BEGIN
 	RETURN query
 	
-	SELECT * FROM trees_watered WHERE trees_watered.tree_id = t_id;
+	SELECT amount, timestamp, username, id, tree_id FROM trees_watered WHERE trees_watered.tree_id = t_id;
 		
 END;
 $function$;
@@ -19,14 +19,14 @@ $function$;
 -- Allow select waterings for a specific user only via rpc
 -- SECURITY DEFINER bypasses the RLS policy
 CREATE OR REPLACE FUNCTION public.waterings_for_user(u_id text)
- RETURNS TABLE(uuid text, amount numeric, "timestamp" timestamptz, username text, id int4, tree_id text)
+ RETURNS TABLE(amount numeric, "timestamp" timestamptz, username text, id int4, tree_id text)
  LANGUAGE plpgsql
  SECURITY DEFINER
 AS $function$
 BEGIN
 	RETURN query
 	
-	SELECT * FROM trees_watered WHERE trees_watered.uuid = u_id;
+	SELECT amount, timestamp, username, id, tree_id FROM trees_watered WHERE trees_watered.uuid = u_id;
 		
 END;
 $function$;
