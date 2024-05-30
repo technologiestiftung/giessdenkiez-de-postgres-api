271d9a185e57ce0b4f0fe1d10a785a7bf90fe67e
Jonas Jaszkowic <jonas.jaszkowic@ts.berlin>
2024-05-30 15:47:35 +0200

    fix: change tree_id to gml_id

--- supabase/migrations/20240530123349_fix_id_columns.sql
+++ supabase/migrations/20240530123349_fix_id_columns.sql
@@ -1,3 +1,9 @@
+UPDATE trees_watered SET tree_id = (SELECT gml_id FROM trees WHERE trees.id = trees_watered.tree_id);
+UPDATE trees_adopted SET tree_id = (SELECT gml_id FROM trees WHERE trees.id = trees_adopted.tree_id);
+
+ALTER TABLE trees_watered RENAME COLUMN tree_id TO gml_id;
+ALTER TABLE trees_adopted RENAME COLUMN tree_id TO gml_id;
+
 ALTER TABLE trees_adopted DROP CONSTRAINT fk_trees_adopted_trees;
 ALTER TABLE trees_watered DROP CONSTRAINT fk_trees_watered_trees;
 ALTER TABLE trees DROP CONSTRAINT trees_pkey;
@@ -7,14 +13,14 @@ ALTER TABLE trees ADD PRIMARY KEY (gml_id);
 
 ALTER TABLE trees_adopted
 ADD CONSTRAINT fk_trees_adopted_trees
-FOREIGN KEY (tree_id)
+FOREIGN KEY (gml_id)
 REFERENCES trees (gml_id)
 ON DELETE CASCADE
 ON UPDATE CASCADE;
 
 ALTER TABLE trees_watered
 ADD CONSTRAINT fk_trees_watered_trees
-FOREIGN KEY (tree_id)
+FOREIGN KEY (gml_id)
 REFERENCES trees (gml_id)
 ON DELETE CASCADE
 ON UPDATE CASCADE;
@@ -22,5 +28,3 @@ ON UPDATE CASCADE;
 DROP TRIGGER insert_uuid on public.trees;
 
 DROP FUNCTION public.uuid_insert_trigger;
-
-
