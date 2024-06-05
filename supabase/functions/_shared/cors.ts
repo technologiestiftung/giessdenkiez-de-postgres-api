3675ff35287d6f9afdc54adb169e91b4378e7f03
Jonas Jaszkowic <jonas.jaszkowic@ts.berlin>
2024-06-05 10:29:05 +0200

    feat: cors via env

--- supabase/functions/_shared/cors.ts
+++ supabase/functions/_shared/cors.ts
@@ -1,5 +1,7 @@
+const ALLOWED_ORIGIN = Deno.env.get("ALLOWED_ORIGIN");
+
 export const corsHeaders = {
-	"Access-Control-Allow-Origin": "http://localhost:5173",
+	"Access-Control-Allow-Origin": ALLOWED_ORIGIN,
 	"Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
 	"Access-Control-Allow-Headers":
 		"Content-Type,Authorization,x-client-info,apikey",
