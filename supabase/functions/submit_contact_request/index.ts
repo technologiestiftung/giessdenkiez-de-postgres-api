ed71156de7642e9fb7c9b699e00c3f8d05ca555d
Jonas Jaszkowic <jonas.jaszkowic@ts.berlin>
2024-06-10 16:45:37 +0200

    feat: Send via smtp directly

--- supabase/functions/submit_contact_request/index.ts
+++ supabase/functions/submit_contact_request/index.ts
@@ -1,8 +1,13 @@
 import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 import { mailTemplate } from "./mail-template.ts";
 import { corsHeaders } from "../_shared/cors.ts";
+import nodemailer from "npm:nodemailer";
 
-const RESEND_API_KEY = Deno.env.get("RESEND_API_KEY");
+const SMTP_HOST = Deno.env.get("SMTP_HOST");
+const SMTP_USER = Deno.env.get("SMTP_USER");
+const SMTP_PASSWORD = Deno.env.get("SMTP_PASSWORD");
+const SMTP_FROM = Deno.env.get("SMTP_FROM");
+
 const SUPABASE_URL = Deno.env.get("URL");
 const SUPABASE_ANON_KEY = Deno.env.get("ANON_KEY");
 const SUPABASE_SERVICE_ROLE_KEY = Deno.env.get("SERVICE_ROLE_KEY");
@@ -103,39 +108,46 @@ const handler = async (_request: Request): Promise<Response> => {
 	}
 
 	// Send the email
-	const res = await fetch("https://api.resend.com/emails", {
-		method: "POST",
-		headers: {
-			"Content-Type": "application/json",
-			Authorization: `Bearer ${RESEND_API_KEY}`,
-		},
-		body: JSON.stringify({
-			from: "onboarding@resend.dev",
+	try {
+		const transporter = nodemailer.createTransport({
+			host: SMTP_HOST,
+			port: 25,
+			// Use `true` for port 465, `false` for all other ports, see: https://nodemailer.com/
+			secure: false,
+			auth: {
+				user: SMTP_USER,
+				pass: SMTP_PASSWORD,
+			},
+		});
+
+		const mailOptions = {
+			from: SMTP_FROM,
 			to: fullContactData.email,
 			subject: "Du hast eine neue Gieß den Kiez Kontaktanfrage erhalten! 🌳",
 			html: mailTemplate(userContactName, message, fullContactData.email),
-		}),
-	});
+		};
 
-	if (res.status !== 200) {
+		// Send the email
+		const info = await transporter.sendMail(mailOptions);
+		console.log(info);
+
+		// Update the contact request with the email id
+		const { error: updateError } = await supabaseClient
+			.from("contact_requests")
+			.update({
+				contact_mail_id: info.response,
+			})
+			.eq("id", insertData.id);
+
+		if (updateError) {
+			return new Response(undefined, { status: 500, headers: corsHeaders });
+		}
+	} catch (e) {
+		console.log(e);
 		return new Response(undefined, { status: 500, headers: corsHeaders });
 	}
 
-	const emailData = await res.json();
-
-	// Update the contact request with the email id
-	const { error: updateError } = await supabaseClient
-		.from("contact_requests")
-		.update({
-			contact_mail_id: emailData.id,
-		})
-		.eq("id", insertData.id);
-
-	if (updateError) {
-		return new Response(undefined, { status: 500, headers: corsHeaders });
-	}
-
-	return new Response(JSON.stringify(""), {
+	return new Response(JSON.stringify({ code: "contact_request_sent" }), {
 		status: 200,
 		headers: {
 			...corsHeaders,
