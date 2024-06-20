51392d36c45fc789daed6586fad7a64b8b7bdadf
Jonas Jaszkowic <jonas.jaszkowic@ts.berlin>
2024-06-20 11:31:27 +0200

    fix: daily weather data

--- supabase/migrations/20240619130511_montly_rain.sql
+++ supabase/migrations/20240619130511_montly_rain.sql
@@ -1,15 +1,18 @@
-create table if not exists monthly_aggregated_radolan_data (
+create table if not exists daily_weather_data (
     id serial primary key,
-    year int2,
-    month int2,
-    last_harvest_day int2,
-    avg_precipitation_liters_per_sm2 float default 0.0,
-    harvesting_finished boolean default false
-);
-
-create table if not exists monthly_radolan_data_temp (
-    id serial primary key,
-    measured_at date not null,
-    value int2,
-    geom_id int2 references radolan_geometry(id)
+    measure_day timestamp not null,
+    day_finished boolean not null default false,
+    sum_precipitation_mm_per_sqm float,
+    avg_temperature_celsius float,
+    avg_pressure_msl float,
+    sum_sunshine_minutes float,
+    avg_wind_direction_deg float,
+    avg_wind_speed_kmh float,
+    avg_cloud_cover_percentage float,
+    avg_dew_point_celcius float,
+    avg_relative_humidity_percentage float,
+    avg_visibility_m float,
+    avg_wind_gust_direction_deg float,
+    avg_wind_gust_speed_kmh float,
+    source_dwd_station_ids text[]
 );
